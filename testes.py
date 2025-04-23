2997a27a6429d70e5838a94edeedc7b61dc1dbec
rober122pst <jrobertinhor.9@gmail.com>
2025-04-23 10:32:48 -0300

    classes

--- testes.py
+++ testes.py
@@ -1,8 +1,8 @@
 while True:
     print("Welcome to the VW and rem converter!")
     pixels = float(input("Enter the number of pixels: "))
-    vw = (pixels*(2/3))/19.2
-    rem = (pixels*(1/3))/16
+    vw = (pixels*(1/3))/19.2
+    rem = (pixels*(2/3))/16
     print(f"The number of vw is: {vw:.2f}")
 
     print(f"The number of rem is: {rem:.2f}")
