a95a6ae99e68658e75dad6d56bf7c28e81cc28d6
rober122pst <jrobertinhor.9@gmail.com>
2025-04-24 00:14:50 -0300

    prototipo-feito

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,19 @@ function navbarColor() {
         header.style.background = 'transparent';
     }
 }
+
+function observeSection() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) { // Trigger when any part of the div is visible
+                entry.target.classList.remove('section-hidden');
+                entry.target.classList.add('section-show');
+            }
+        });
+    }, { threshold: 0 }); // Threshold 0 means any visible part will trigger
+
+    const sections = document.querySelectorAll('.section-hidden');
+    sections.forEach(section => observer.observe(section));
+}
+
+observeSection();
